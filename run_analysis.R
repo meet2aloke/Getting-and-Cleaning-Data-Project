8c440683a57a6f2d96748869e8f772d3421bfe5c
Aloke Maity <meet2aloke@gmail.com>
2015-04-24 13:00:01 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,8 +3,8 @@ setwd("C:/Users/amaity1/Getting and Cleaning Data/Project/UCI HAR Dataset")
 library(plyr)
 
 ###Part-1
-###=======================================================================================
 ###Merges the training and the test sets to create one data set.
+###=======================================================================================
 x_train <- read.table("train/X_train.txt") 
 y_train <- read.table("train/y_train.txt") 
 subject_train <- read.table("train/subject_train.txt") 
@@ -21,8 +21,8 @@ subject_data <- rbind(subject_train, subject_test)
 
 
 ###Part-2
-###=======================================================================================
 ###Extracts only the measurements on the mean and standard deviation for each measurement. 
+###=======================================================================================
 features <- read.table("features.txt") 
 # get only columns with mean() or std() in their names 
 mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2]) 
@@ -33,8 +33,8 @@ names(x_data) <- features[mean_and_std_features, 2]
 
 
 ###Part-3
-###=======================================================================================
 ###Uses descriptive activity names to name the activities in the data set
+###=======================================================================================
 activities <- read.table("activity_labels.txt") 
 # update values with correct activity names 
 y_data[, 1] <- activities[y_data[, 1], 2] 
@@ -43,8 +43,8 @@ names(y_data) <- "activity"
 
 
 ###Part-4
-###=======================================================================================
 ###Appropriately labels the data set with descriptive variable names. 
+###=======================================================================================
 # correct column name 
 names(subject_data) <- "subject" 
 # bind all the data in a single data set 
@@ -52,8 +52,8 @@ all_data <- cbind(x_data, y_data, subject_data)
 
 
 ###Part-5
-###=======================================================================================
 ###From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+###=======================================================================================
 # 66 <- 68 columns but last two (activity & subject) 
 averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66])) 
 write.table(averages_data, "averages_data.txt", row.name=FALSE)
